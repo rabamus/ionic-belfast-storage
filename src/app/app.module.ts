335b8681c8ff5be7d819abe17084892b125e4738
Rob Sinton <rob@tepee.digital>
2017-10-24 17:11:49 +0100

    Quick refactor of initial blank app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,12 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
 
 @NgModule({
   declarations: [
+    ListPage,
     MyApp,
-    HomePage
   ],
   imports: [
     BrowserModule,
@@ -18,13 +18,13 @@ import { HomePage } from '../pages/home/home';
   ],
   bootstrap: [IonicApp],
   entryComponents: [
+    ListPage,
     MyApp,
-    HomePage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
 export class AppModule {}
